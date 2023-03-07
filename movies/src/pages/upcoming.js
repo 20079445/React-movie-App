a42cda407556000731a9113ba315ab25980b3576
jack murphy <20079445@mail.wit.ie>
2023-03-07 10:57:54 +0000

    fix upcoming movies page

--- movies/src/pages/upcoming.js
+++ movies/src/pages/upcoming.js
@@ -1,8 +1,14 @@
 import React, { useState, useEffect } from "react";
 import PageTemplate from '../components/templateMovieListPage'
 import { getUpcomingMovies } from "../api/tmdb-api";
+import { useQuery } from 'react-query';
+import Spinner from '../components/spinner';
+import AddToFavoritesIcon from '../components/cardIcons/addToFavorites'
+
+
 
 const UpcomingMoviesPage = (props) => {
+
   const [upcoming, setMovies] = useState([]);
   const favorites = upcoming.filter(m => m.favorite)
   localStorage.setItem('favorites', JSON.stringify(favorites))
@@ -21,12 +27,26 @@ const UpcomingMoviesPage = (props) => {
     });
   }, []);
 
+  const {  data, error, isLoading, isError }  = useQuery('upcoming', getUpcomingMovies)
+
+  if (isLoading) {
+    return <Spinner />
+  }
+
+  if (isError) {
+    return <h1>{error.message}</h1>
+  }  
+  const movies = data.results;
+
 
 return (
     <PageTemplate
       title='Upcoming Movies'
       movies={upcoming}
       selectFavorite={addToFavorites}
+      action={(movie) => {
+        return <AddToFavoritesIcon movie={movie} />
+      }}
     />
   );
 }
