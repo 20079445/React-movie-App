eaf18ca18475f9a759d98f6ea3de5d0c3c2b8194
jack murphy <20079445@mail.wit.ie>
2023-03-07 10:34:24 +0000

    Added new review support

--- movies/src/index.js
+++ movies/src/index.js
@@ -10,6 +10,7 @@ import UpcomingMoviesPage from "./pages/upcoming";
 import { QueryClientProvider, QueryClient } from "react-query";
 import { ReactQueryDevtools } from 'react-query/devtools';
 import MoviesContextProvider from "./contexts/moviesContext";
+import AddMovieReviewPage from './pages/addMovieReviewPage'
 
 
 
@@ -35,6 +36,7 @@ const App = () => {
           <Route path="/movies/upcoming" element={<UpcomingMoviesPage />} />
           <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
           <Route path="/movies/:id" element={<MoviePage />} />
+          <Route path="/reviews/form" element={ <AddMovieReviewPage /> } />
           <Route path="/" element={<HomePage />} />
           <Route path="*" element={ <Navigate to="/" /> } />
         </Routes>
--- movies/src/pages/favoriteMoviesPage.js
+++ movies/src/pages/favoriteMoviesPage.js
@@ -32,7 +32,7 @@ const FavoriteMoviesPage = () => {
     return q.data
   });
 
-  const toDo = () => true;
+  //const toDo = () => true;
 
   return (
     <PageTemplate
